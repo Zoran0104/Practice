2045827b6ba711aa2586fb5426f2716ff5565354
Zoran <zwyliufeng@outlook.com>
2016-11-17 19:33:57 +0800

    zoran

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -4,20 +4,20 @@ long loop1(int);
 long loop2(int);
 long loop3(int);
 long digui(int);
-long digui2(int);
+
 int main(void)
 {
 	int thenum;
 	printf("请输入一个小于12的数字：");
 	while (scanf_s("%d", &thenum) == 1)
 	{
-		if (thenum <= 0 || thenum > 12)
+		if (thenum <= 0 || thenum > 15)
 		{
 			printf("您输入的数字不符合规范，请重新输入：");
 			continue;
 		}
 		printf("loop1:%ld,loop2:%ld,loop3:%ld\n"
-			"digui1:%ld,digui2:%ld,digui3:%ld",loop1(thenum),loop2(thenum),loop3(thenum),digui(thenum),digui2(thenum));
+			"digui1:%ld,digui2:",loop1(thenum),loop2(thenum),loop3(thenum),digui(thenum));
     }
 }
 
@@ -27,7 +27,7 @@ long loop1(int a)
 	long sum=1;
 	while (i <= a)
 	{
-		sum *= a;
+		sum *= i;
 		i++;
 	}
 	return sum;
@@ -66,14 +66,3 @@ long digui(int a)
 	return sum;
 }
 
-long digui2(int a)
-{
-	long sum = a;
-	while (a)
-	{
-		sum *= digui(a--);
-	}
-	if (a == 0)
-		sum = 1;
-	return sum;
-}
