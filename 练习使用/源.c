87e89a3c1008a7516d3d42de69636c096a7455c7
Zoran <zwyliufeng@outlook.com>
2016-11-17 19:21:18 +0800

    zoran

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,54 +1,79 @@
 #include<stdio.h>
-int loop(int);
-int digui(int);
+#include<math.h>
+long loop1(int);
+long loop2(int);
+long loop3(int);
+long digui(int);
+long digui2(int);
 int main(void)
 {
 	int thenum;
-	long di;
-	printf("请输入一个0~12之间的数字：");
+	printf("请输入一个小于12的数字：");
 	while (scanf_s("%d", &thenum) == 1)
 	{
-		if (thenum <= 0)
+		if (thenum <= 0 || thenum > 12)
 		{
-			printf("您输入的数字小于0，请重新输入：");
+			printf("您输入的数字不符合规范，请重新输入：");
 			continue;
 		}
-		else if (thenum > 12)
-		{
-			printf("您输入的数字过大，请重新输入：");
-			continue;
-		}
-		else
-		{
-			loop(thenum);
-			di = digui(thenum);
-		}
-		printf("使用递归求得的值是：%ld\n", di);
-	}
-
-	system("pause");
-	
+		printf("loop1:%ld,loop2:%ld,loop3:%ld\n"
+			"digui1:%ld,digui2:%ld,digui3:%ld",loop1(thenum),loop2(thenum),loop3(thenum),digui(thenum),digui2(thenum));
+    }
 }
 
-int loop(int a)
+long loop1(int a)
+{
+	int i = 1;
+	long sum=1;
+	while (i <= a)
+	{
+		sum *= a;
+		i++;
+	}
+	return sum;
+}
+
+long loop2(int a)
+{
+	int i=1;
+	long sum=1;
+	do {
+		sum *= i;
+		i++;
+	} while (i <= a);
+	return sum;
+}
+
+long loop3(int a)
 {
 	int i;
-	long sum = 1;
-	for (i=1; i <=a; i++)
+	long sum=1;
+	for (i = 1; i <= a; i++)
 	{
 		sum *= i;
 	}
-	printf("使用循环求得的值是：%ld\n", sum);
-	return 0;
+	return sum;
 }
 
 long digui(int a)
 {
-	long sum;
-	if (a >1)
+	long sum=a;
+	if (a>0)
 	{
-		sum =a*digui(a - 1);
+		sum *= digui(a - 1);
 	}
 	else sum = 1;
+	return sum;
+}
+
+long digui2(int a)
+{
+	long sum = a;
+	while (a)
+	{
+		sum *= digui(a--);
+	}
+	if (a == 0)
+		sum = 1;
 	return sum;
 }
