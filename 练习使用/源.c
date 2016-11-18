2fe6c1ef52e4ca1f657257d01dc68f34ff546ce0
Zoran <zwyliufeng@outlook.com>
2016-11-18 11:38:48 +0800

    11.18

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,68 +1,46 @@
 #include<stdio.h>
-#include<math.h>
-long loop1(int);
-long loop2(int);
-long loop3(int);
-long digui(int);
+double loop(int a);
+double loop2(int a);
+double digui1(int a);
 
 int main(void)
 {
-	int thenum;
-	printf("请输入一个小于12的数字：");
-	while (scanf_s("%d", &thenum) == 1)
+	int innum;
+	printf("请输入一个0-12的数字：");
+	while (scanf_s("%d", &innum) == 1)
 	{
-		if (thenum <= 0 || thenum > 15)
+		if (innum > 12 || innum < 0)
 		{
-			printf("您输入的数字不符合规范，请重新输入：");
+			printf("您输入的数字有误，请重新输入：");
 			continue;
 		}
-		printf("loop1:%ld,loop2:%ld,loop3:%ld\n"
-			"digui1:%ld,digui2:",loop1(thenum),loop2(thenum),loop3(thenum),digui(thenum));
-    }
-}
+		printf("loop1:%-3.0lf,loop2:%-3.0lf,", loop(innum), loop2(innum));
 
-long loop1(int a)
-{
-	int i = 1;
-	long sum=1;
-	while (i <= a)
-	{
-		sum *= i;
-		i++;
 	}
-	return sum;
+
+
 }
 
-long loop2(int a)
+double loop(int a)
 {
-	int i=1;
-	long sum=1;
-	do {
-		sum *= i;
-		i++;
-	} while (i <= a);
-	return sum;
-}
-
-long loop3(int a)
-{
-	int i;
-	long sum=1;
-	for (i = 1; i <= a; i++)
+	double outnum = 1;
+	while(a)
 	{
-		sum *= i;
+		outnum *= a;
+		a--;
 	}
-	return sum;
+	return outnum;
 }
 
-long digui(int a)
+double loop2(int a)
 {
-	long sum=a;
-	if (a>0)
-	{
-		sum *= digui(a - 1);
-	}
-	else sum = 1;
-	return sum;
+	double outnum = 1;
+	for (; a > 0; a--)
+		outnum *= a;
+	return outnum;
 }
 
+double digui1(int a)
+{
+	return 0.0;
+}
