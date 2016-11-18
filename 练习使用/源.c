8ce7d7e0af15a863cf677c2f2da7af283ebbe559
Zoran <zwyliufeng@outlook.com>
2016-11-18 19:21:37 +0800

    11.18

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -2,23 +2,23 @@
 int main(void)
 {
 	int i, j, n;
-	printf("Please Enter n: "); 
-	scanf_s("%d", &n); 
-	for (i = 1;i <= n;i++)
+	while (scanf_s("%d", &n) == 1)
 	{
-		for (j = 1; j <= n + i - 1; j++)
-			if (j > n - i) 
-				printf("*"); 
-			else printf(" "); 
-			printf("\n");
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n + i - 1; j++)
+				if (j > n - i)
+					printf("*");
+				else printf(" ");
+				printf("\n");
+		}
+		for (i = 1; i < n; i++)
+		{
+			for (j = 1; j <= 2 * n - i - 1; j++)
+				if (j > i)
+					printf("*");
+				else printf(" ");
+				printf("\n");
+		}
 	}
-	for (i = 1;i<n;i++)
-	{
-		for (j = 1; j <= 2 * n - i - 1; j++) 
-			if (j > i) 
-				printf("*"); 
-			else printf(" "); 
-			printf("\n");
-	}
-	system("pause");
 }
