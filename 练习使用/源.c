1e8c1b915d8eb02822fa9bfab58f828bb28686b8
Zoran <zwyliufeng@outlook.com>
2016-11-18 19:02:11 +0800

    11.18

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 int main(void)
 {
-	int i, k;
-	for (i = 1; i <= 9; i++)
+	int i, j,k;
+	for (i = 1; i <= 9; i=i+2)
 	{
+		for (j = 1; j <= (9-i)/2; j++)
+		{
+			printf(" ");
+		}
 		for (k = 1; k <= i; k++)
-			{
-				printf("%d¡Á%d=%-2d   ", k, i, k*i);
-			}
-			printf("\n");
-		
+			printf("*");
+		printf("\n");
+	}
+	for (i = 9; i > 0; i = i - 2)
+	{
+		for (j = 1; j <= (9 - i) / 2; j++)
+		{
+			printf(" ");
+		}
+		for (k = 1; k <= i; k++)
+			printf("*");
+		printf("\n");
 	}
 	system("pause");
 }
