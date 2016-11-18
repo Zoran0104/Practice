315aa86384a1cf11c6f1ab8f9ff6d5aab4efb3da
Zoran <zwyliufeng@outlook.com>
2016-11-18 18:28:09 +0800

    11.18

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,61 +1,15 @@
 #include<stdio.h>
-double loop(int a);
-double loop2(int a);
-double digui1(int a);
-double digui2(int a);
-
 int main(void)
 {
-	int innum;
-	printf("请输入一个0-12的数字：");
-	while (scanf_s("%d", &innum) == 1)
+	int i, k;
+	for (i = 1; i <= 9; i++)
 	{
-		if (innum > 12 || innum < 0)
-		{
-			printf("您输入的数字有误，请重新输入：");
-			continue;
-		}
-		printf("loop1:%-3.0lf,loop2:%-3.0lf,digui1=%-3.0lf", loop(innum), loop2(innum),digui1(innum));
-
+		for (k = 1; k <= i; k++)
+			{
+				printf("%d×%d=%-2d   ", k, i, k*i);
+			}
+			printf("\n");
+		
 	}
-
-
-}
-
-double loop(int a)
-{
-	double outnum = 1;
-	while(a)
-	{
-		outnum *= a;
-		a--;
-	}
-	return outnum;
-}
-
-double loop2(int a)
-{
-	double outnum = 1;
-	for (; a > 0; a--)
-		outnum *= a;
-	return outnum;
-}
-
-double digui1(int a)
-{
-	double outnum=a;
-	if (a > 0)
-		outnum =outnum* digui1(a - 1);
-	else outnum = 1;
-	return outnum;
-}
-
-double digui2(int a)
-{
-	double sum = a;
-	while (a)
-	{
-		sum *= digui2(a - 1);
-	}
-	return sum;
+	system("pause");
 }
