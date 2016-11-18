df57f55399a6b1edb62c087aa76642a8fda97eca
Zoran <zwyliufeng@outlook.com>
2016-11-18 19:08:20 +0800

    11.18

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,26 +1,24 @@
 #include<stdio.h>
 int main(void)
 {
-	int i, j,k;
-	for (i = 1; i <= 9; i=i+2)
+	int i, j, n;
+	printf("Please Enter n: "); 
+	scanf_s("%d", &n); 
+	for (i = 1;i <= n;i++)
 	{
-		for (j = 1; j <= (9-i)/2; j++)
-		{
-			printf(" ");
-		}
-		for (k = 1; k <= i; k++)
-			printf("*");
-		printf("\n");
+		for (j = 1; j <= n + i - 1; j++)
+			if (j > n - i) 
+				printf("*"); 
+			else printf(" "); 
+			printf("\n");
 	}
-	for (i = 9; i > 0; i = i - 2)
+	for (i = 1;i<n;i++)
 	{
-		for (j = 1; j <= (9 - i) / 2; j++)
-		{
-			printf(" ");
-		}
-		for (k = 1; k <= i; k++)
-			printf("*");
-		printf("\n");
+		for (j = 1; j <= 2 * n - i - 1; j++) 
+			if (j > i) 
+				printf("*"); 
+			else printf(" "); 
+			printf("\n");
 	}
 	system("pause");
 }
