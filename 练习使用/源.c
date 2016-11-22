833938cf02b1a45f4c1fb04b6078f171090f60c8
Zoran <zwyliufeng@outlook.com>
2016-11-22 12:13:14 +0800

    11.22

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
-int main(void)
-{
-	char fname[40];
-	char sname[40];
-	printf("Please enter your first name:\n");
-	scanf_s("%s", fname);
-	printf("Please enter your second name:\n");
-	scanf_s("%s", sname);
-	printf("%s,%s", fname, sname);
-	system("pause");
-}
+#include <stdio.h>
+int main(void) 
+{     
+	char fname[40];     
+	char lname[40];          
+	printf("Enter your first name: ");     
+	scanf_s("%s", fname);     
+	printf("Enter your last name: ");     
+	scanf_s("%s", lname);     
+	printf("%s, %s\n", lname, fname);     
+	return 0; } 
