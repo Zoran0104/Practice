eb930b6473c0457f057e06fb67d9650754f88e91
Zoran <zwyliufeng@outlook.com>
2016-11-22 11:02:30 +0800

    11:02

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,46 +1,12 @@
 #include<stdio.h>
-double loop1(int a);
-double loop2(int a);
-double loop3(int a);
 int main(void)
 {
-	int num;
-	printf("Please enter a number between 1 to 12:");
-	while (scanf_s("%d", &num) == 1)
-	{
-		if (num > 12 || num <= 0) 
-		{
-			printf("Rewrite a number:");
-			continue;
-		}
-		printf("loop1:%11.2lf;loop2:%11.2lf;loop3:%11.2lf",loop1(num),loop2(num),loop3(num));
-	}
-
-}
-double loop1(int a)
-{
-	double sum=1;
-	for (; a>0; a--)
-	{
-		sum *= a;
-	}
-	return sum;
-}
-double loop2(int a)
-{
-	double sum = 1;
-	while (a)
-	{
-		sum *= a;
-		a--;
-	}
-	return sum;
-}
-double loop3(int a)
-{
-	double sum=a;
-	if (a)
-		sum *= loop3(a - 1);
-	else sum = 1;
-	return sum;
+	char fname[40];
+	char sname[40];
+	printf("Please enter your first name:\n");
+	scanf_s("%s", fname);
+	printf("Please enter your second name:\n");
+	scanf_s("%s", sname);
+	printf("%s,%s", fname, sname);
+	system("pause");
 }
