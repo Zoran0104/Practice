4124016a2ac2100cb020cff2e4a0b82b5e0a1b4f
Zoran <zwyliufeng@outlook.com>
2016-11-22 16:00:20 +0800

    1234

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-int main(void)
+int main()
 {
-	printf("Type int has a size of %zd bytes.\n", sizeof(int));
-	printf("Type double has size of %zd bytes.\n", sizeof(double));
-	printf("Type char has size of %zd bytes.\n", sizeof(char));
-	printf("Type long long has size of %zd bytes.\n", sizeof(long long));
-	printf("Type long double has size of %zd bytes.", sizeof(long double));
-	getchar();
+	char char1;
+	printf("enter a num you want to show the char:________\b\b\b\b\b\b\b\b");
+	while (scanf_s("%d",&char1)==1)
+	{
+		printf("%c\n", char1);
+		printf("enter a num you want to show the char:________\b\b\b\b\b\b\b\b");
+	}
 }
