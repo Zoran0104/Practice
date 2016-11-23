2df9d6357b22416c6e99b0d3ceb10b5d5c3730f2
Zoran <zwyliufeng@outlook.com>
2016-11-23 22:00:20 +0800

    11.23

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
-int main()
+void replace(int *a, int *b);
+int main(void) {
+	int a, b;
+	scanf_s("%d%d", &a, &b);
+	replace(&a, &b);
+	printf("%d %d", a, b);
+	system("pause");
+}
+void replace(int *a, int *b)
 {
-	char char1;
-	printf("enter a num you want to show the char:________\b\b\b\b\b\b\b\b");
-	while (scanf_s("%d",&char1)==1)
-	{
-		printf("%c\n", char1);
-		printf("enter a num you want to show the char:________\b\b\b\b\b\b\b\b");
-	}
+	int t;
+	t = *a;
+	*a = *b;
+	*b = t;
 }
