96f172600f5ca4b58f4e522abcef22674f9ee4c0
Zoran <zwyliufeng@outlook.com>
2016-11-24 21:16:40 +0800

    11.24

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
-void replace(int *a, int *b);
-int main(void) {
-	int a, b;
-	scanf_s("%d%d", &a, &b);
-	replace(&a, &b);
-	printf("%d %d", a, b);
-	system("pause");
-}
-void replace(int *a, int *b)
+#define SIZE 10;
+int main(void)
 {
-	int t;
-	t = *a;
-	*a = *b;
-	*b = t;
+	int score[10];
+	int i,sum=0;
+	for (i = 0; i < 10; i++)
+		scanf_s("%d", &score[i]);
+	printf("您输入的分数如下：\n");
+	for (i = 0; i < 10; i++)
+	{
+		printf("%5d", score[i]);
+		sum += score[i];
+	}
+	printf("\n您输入的成绩的总和为%d平均值为%d", sum, (double)sum /10);
+	system("pause");
+	
+	
 }
