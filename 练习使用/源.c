68a0a42c895abffb8aca5cf04281ca3b90b19b74
Zoran <zwyliufeng@outlook.com>
2016-11-25 20:04:16 +0800

    11.25

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-#include<ctype.h>
-#include<stdbool.h>
-#define STOP '|'
+#include<ctype.h>												//为isspace()函数提供原型
+#include<stdbool.h>												//为bool true、false提供定义
+#define STOP '|'												
 int main(void)
 {
 	char c;
 	char prev;
-	long n_chars = 0;
-	int n_lines = 0;
-	int n_words = 0;
-	int p_lines = 0;
-	bool inword = false;
+	long n_chars = 0;													//字符数
+	int n_lines = 0;													//行数
+	int n_words = 0;									//单词数
+	int p_lines = 0;						//不完整的行数
+	bool inword = false;       //如果在单词中，inword等于true
 	printf("Enter text to be analyzed(| to terminate):\n");
 	prev = '\n';
 	while ((c = getchar()) != STOP)
