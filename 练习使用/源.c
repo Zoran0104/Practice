6b1ba922a4861048c4122c9f385a539b7c2a555c
Zoran <zwyliufeng@outlook.com>
2016-11-26 21:49:38 +0800

    11.26

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,36 +1,31 @@
 #include<stdio.h>
-#include<ctype.h>												//为isspace()函数提供原型
-#include<stdbool.h>												//为bool true、false提供定义
-#define STOP '|'												
+#include<stdbool.h>
+#include<ctype.h>
+#define STOP '|'
 int main(void)
 {
+	int words=0;
+	long chars = 0;
+	int lines = 0;
+	int prelines = 0;
 	char c;
-	char prev;
-	long n_chars = 0;											//字符数
-	int n_lines = 0;											//行数
-	int n_words = 0;											//单词数
-	int p_lines = 0;											//不完整的行数
-	bool inword = false;										 //如果在单词中，inword等于true
-	printf("Enter text to be analyzed(| to terminate):\n");
-	prev = '\n';												//初始化prev值
+	char pre = '\n';
+	bool inword = false;
 	while ((c = getchar()) != STOP)
 	{
-		n_chars++;												
-		if (c == '\n')
-			n_lines++;
-		if (!isspace(c) && !inword)
+		chars++;
+		c == '\n' ? lines++ :1;
+		if (!inword && !isspace(c))
 		{
+			words++;
 			inword = true;
-			n_words++;
 		}
-		if (isspace(c) && inword)
+		if (inword&&isspace(c))
 			inword = false;
-		prev = c;
+		pre = c;
 	}
-	if (prev != '\n')
-		p_lines = 1;
-	printf("characters=%ld,words=%d,lines=%d,", n_chars, n_words, n_lines);
-	printf("partial lines=%d\n", p_lines);
+	if (pre != '\n')
+		prelines++;
+	printf("字符数：%ld，单词数：%d，行数：%d，未完成行数：%d。", chars, words, lines, prelines);
 	system("pause");
-	return 0;
 }
