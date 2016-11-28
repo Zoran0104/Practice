c77063d3d41800db1227161e7b4da429c354db41
Zoran <zwyliufeng@outlook.com>
2016-11-28 20:50:12 +0800

    11.28

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -4,28 +4,30 @@
 #define STOP '|'
 int main(void)
 {
-	int words=0;
+	bool inword;
+	char c;
 	long chars = 0;
+	int words = 0;
 	int lines = 0;
 	int prelines = 0;
-	char c;
 	char pre = '\n';
-	bool inword = false;
+	inword = false;
 	while ((c = getchar()) != STOP)
 	{
 		chars++;
-		c == '\n' ? lines++ :1;
+		c == '\n' ? lines++ : 1;
 		if (!inword && !isspace(c))
 		{
-			words++;
 			inword = true;
+			words++;
 		}
 		if (inword&&isspace(c))
+		{
 			inword = false;
+		}
 		pre = c;
 	}
-	if (pre != '\n')
-		prelines++;
-	printf("字符数：%ld，单词数：%d，行数：%d，未完成行数：%d。", chars, words, lines, prelines);
+	pre = '\n' ? prelines = 1 : 1;
+	printf("chars=%ld,lines=%d,words=%d,prelines=%d", chars, lines, words, prelines);
 	system("pause");
 }
