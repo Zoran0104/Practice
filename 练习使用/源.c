ef9cfe91528de464fce982e604e154b0a2cfcbc5
Zoran <zwyliufeng@outlook.com>
2016-12-01 21:27:25 +0800

    12.1

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,12 +1,35 @@
 #include<stdio.h>
-int main(void)
+#include<ctype.h>
+#include<stdbool.h>
+#define STOP '@'
+int  main(void)
 {
-	int i;
-	for (i = 'A'; i < 'A'+26; i++)
-		printf("%c", i);
-	putchar('\n');
-	for (i = 'Z'; i >= 'A'; i--)
-		printf("%c", i);
-	putchar('\n');
-	system("pause");
+	while (true)
+	{
+		char c;
+		char prechar = '\n';
+		int preline = 0;
+		int lines = 0;
+		int words = 0;
+		long chars = 0;
+		bool inword = false;
+		while ((c = getchar()) != STOP)
+		{
+			chars++;
+			c == '\n' ? lines++ : 1;
+			if (!isspace(c) && !inword && !ispunct(c))
+			{
+				words++;
+				inword = true;
+			}
+			if (isspace(c) && inword)
+			{
+				inword = false;
+			}
+			prechar = c;
+		}
+		prechar == '\n' ? 1 : preline++;
+		printf("chars=%ld,words=%d,lines=%d", chars, words, lines);
+		preline == 0 ? printf(".\n") : printf(",preline=1.\n");
+	}
 }
