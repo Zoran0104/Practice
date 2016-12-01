e2431bc1f2d50ee7daf3f5ad595a10326ec26465
Zoran <zwyliufeng@outlook.com>
2016-12-01 20:52:26 +0800

    12.1

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,34 +1,12 @@
 #include<stdio.h>
-#include<stdbool.h>
-#include<ctype.h>
-#define STOP '|'
 int main(void)
 {
-
-	char c;
-	int words=0;
-	long chars=0;
-	int lines = 0;
-	int prelines = 0;
-	bool inword = false;
-	char pre = '\n';
-	while ((c = getchar()) != STOP)
-	{
-		chars++;
-		c == '\n' ? lines++ : 1;
-		if (!inword && !isspace(c))
-		{
-			inword = true;
-			words++;
-		}
-		if (inword&&isspace(c))
-		{
-			inword = false;
-		}
-		pre = c;
-	}
-	pre == '\n'? 1 : prelines++;
-	printf("chars:%ld,lines:%d,words=%d,prelines=%d", chars, lines, words, prelines);
-
-
+	int i;
+	for (i = 'A'; i < 'A'+26; i++)
+		printf("%c", i);
+	putchar('\n');
+	for (i = 'Z'; i >= 'A'; i--)
+		printf("%c", i);
+	putchar('\n');
+	system("pause");
 }
