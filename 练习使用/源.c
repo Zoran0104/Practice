66913274e2b9ba63c3ae9b76bb79dc63973a11ba
Zoran <zwyliufeng@outlook.com>
2016-12-02 22:38:58 +0800

    122

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,28 +1,46 @@
 #include<stdio.h>
+int strlens(const char *s);
+char strcpys(char *s, const char *ss);
+int strcmps(const char *s, const char *ss);
+char strcats(char *s, char *ss);
+
 int main(void)
 {
-	int i;
-	int a[10];
-	int max;
-	int temp=0;
-	int tempnum;
-	for (i = 0; i < 10; i++)
-		scanf_s("%d", &a[i]);
-	max = a[0];
-	for (i = 1; i < 10; i++)
-	{
-		if (max < a[i])
-		{
-			max = a[i];
-			temp = i;
-		}
-	}
-	printf("max=%d\n", max);
-	tempnum = a[temp];
-	a[temp] = a[9];
-	a[9] = tempnum;
-	printf("重新排序后的数组为：");
-	for (i = 0; i < 10; i++)
-		printf("%-3d", a[i]);
-	system("pause");
 }
+	
+
+
+int strlens(const char *s)
+{
+	int len = 0;
+	while (*s++)
+		len++;
+	return len+1;
+}
+
+char strcpys(char * s, const char * ss)
+{
+	while (*s++ = *ss++);
+	return s;
+}
+int strcmps(const char *s, const char *ss)
+{
+	char *s3 = s, *s4 = ss;
+	while (*s3++ != *s4++ && (*s3 || *s4))
+	{
+		if (*--s3<*--s4)
+			return -1;
+		else if (*s3>*s4) return 1;
+		else return 0;
+		s3++; s4++;
+	}
+}
+char strcats(char *s, char *ss)  
+{
+	char *q = s;
+	while (*(s++));
+	s--;
+	while ((*(s++) = *ss, *(ss++)));
+	return q;
+}
+
