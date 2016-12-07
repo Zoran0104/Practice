cb21e1c651c8313708eb82d3797328cce628dbbb
Zoran <zwyliufeng@outlook.com>
2016-12-07 20:34:23 +0800

    --

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,29 +1,45 @@
+/*#include<stdio.h>
+#include<ctype.h>
+#define STOP '|'
+int main(void)
+{
+	while (1)
+	{
+		char a[50];
+		char b[50];
+		char c;
+		int i = 0;
+		int j = 0;
+		printf("请输入您要筛选的字符串：以‘|’结束:\n");
+		while ((c = getchar()) != STOP)
+		{
+			a[i] = c;
+			i++;
+		}
+		a[i] = '\0';
+		for (i = 0; i < 50, a[i] != '\0'; i++)
+		{
+			if (islower(a[i]))
+			{
+				b[j] = a[i];
+				j++;
+			}
+		}
+		b[j] = '\0';
+		printf("a的数组里含有的字符是：%s\nb的数组里含有的字符是：%s。\n", a, b);
+	}
+	system("pause");
+}*/
 #include<stdio.h>
 #include<ctype.h>
 #define STOP '|'
 int main(void)
 {
-	char a[50];
-	char b[50];
+	int a[100];
+	int b[100];
 	char c;
-	int i = 0;
-	int j = 0;
-	printf("请输入您要筛选的字符串：以‘|’结束:\n");
 	while ((c = getchar()) != STOP)
 	{
-		a[i] = c;
-		i++;
+
 	}
-	a[i] = '\0';
-	for (i = 0; i < 50,a[i]!='\0'; i++)
-	{
-		if (islower(a[i]))
-		{
-			b[j] = a[i];
-			j++;
-		}
-	}
-	b[j] = '\0';
-	printf("a的数组里含有的字符是：%s\nb的数组里含有的字符是：%s。", a, b);
-	system("pause");
 }
