00219e26e4bd51a1b2d1521614f2775ead79b9d1
Zoran <zwyliufeng@outlook.com>
2016-12-07 21:47:26 +0800

    1

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,23 +1,25 @@
-/*#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <ctype.h>
 #define STOP '|'
 int main(void)
 {
-	while (1)
+    while (1)
 	{
-		char a[50];
-		char b[50];
-		char c;
-		int i = 0;
-		int j = 0;
-		printf("请输入您要筛选的字符串：以‘|’结束:\n");
+	char a[100];
+	char b[100];
+	char c;
+	int i = 0;
+	int j = 0;
+		printf("请输入您要筛选的字符串（以‘|’结束）：\n");
 		while ((c = getchar()) != STOP)
 		{
 			a[i] = c;
 			i++;
+			if (i == 50)
+				break;
 		}
 		a[i] = '\0';
-		for (i = 0; i < 50, a[i] != '\0'; i++)
+		for (i = 0; i < 100,a[i]!='\0'; i++)
 		{
 			if (islower(a[i]))
 			{
@@ -26,20 +28,6 @@ int main(void)
 			}
 		}
 		b[j] = '\0';
-		printf("a的数组里含有的字符是：%s\nb的数组里含有的字符是：%s。\n", a, b);
-	}
-	system("pause");
-}*/
-#include<stdio.h>
-#include<ctype.h>
-#define STOP '|'
-int main(void)
-{
-	int a[100];
-	int b[100];
-	char c;
-	while ((c = getchar()) != STOP)
-	{
-
+		printf("%s", b);
 	}
 }
