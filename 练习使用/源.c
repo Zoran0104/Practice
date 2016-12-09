4776689c93b0b4eb73d35c82b84c0d00674584e1
Zoran <zwyliufeng@outlook.com>
2016-12-09 15:54:24 +0800

    1209

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,33 +1,16 @@
-#include <stdio.h>
-#include <ctype.h>
-#define STOP '|'
+#include<stdio.h>
+#include<string.h>
 int main(void)
 {
-    while (1)
-	{
 	char a[100];
 	char b[100];
-	char c;
-	int i = 0;
-	int j = 0;
-		printf("请输入您要筛选的字符串（以‘|’结束）：\n");
-		while ((c = getchar()) != STOP)
-		{
-			a[i] = c;
-			i++;
-			if (i == 50)
-				break;
-		}
-		a[i] = '\0';
-		for (i = 0; i < 100,a[i]!='\0'; i++)
-		{
-			if (islower(a[i]))
-			{
-				b[j] = a[i];
-				j++;
-			}
-		}
-		b[j] = '\0';
-		printf("%s", b);
+	int i;
+	printf("请输入一段字符：");
+	scanf("%s", a);
+	printf("请输入另一段字符串：");
+	scanf("%s", b);
+	for (i = 1; i <= strlen(a); i++)
+	{
+		if(strncmp(a,b,5)==0)
 	}
 }
