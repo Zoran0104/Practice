074ee8bf999cdfafa3b30ab5953600a1c22e55c4
Zoran <zwyliufeng@outlook.com>
2016-12-09 22:36:45 +0800

    1209

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -4,35 +4,25 @@ int main(void)
 {
 	char a[100];
 	char b[100];
+	int sa;
+	int sb;
 	int i;
+	int k;
+	int n=0;
 	printf("请输入一段字符：");
 	scanf("%s", a);
 	printf("请输入另一段字符串：");
 	scanf("%s", b);
-	for (i = 1; i <= strlen(a); i++)
+	sa = strlen(a);
+	sb = strlen(b);
+	for (k=0;k<=sa;k++)
 	{
-		if(strncmp(a,b,5)==0)
+		for (i = 0; b[i] != 0; i++)
+			if (b[i] != a[k + 1])
+				break;
+		if (b[i] == 0)
+			n++;
 	}
-}
-#include<stdio.h>
-#include<string.h>
-int main(void)
-{
-	while (1)
-	{
-		char a[100];
-		char b[100];
-		int i;
-		printf("请输入一个字符串：");
-		scanf_s("%s", a, 100);
-		for (i = 0; i < strlen(a); i++)
-		{
-			b[i] = a[strlen(a) - 1 - i];
-		}
-		b[i] = '\0';
-		if (strcmp(a, b) == 0)
-			printf("您输入的字符串是回文字符串。\n");
-		else printf("您输入的字符串不是回文字符串。\n");
-	}
-
+	printf("%d", n);
+	system("pause");
 }
