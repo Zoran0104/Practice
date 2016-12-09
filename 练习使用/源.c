3079f28511166a30baa61a009719134532dc5a6a
Zoran <zwyliufeng@outlook.com>
2016-12-09 15:54:49 +0800

    1209

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -14,3 +14,25 @@ int main(void)
 		if(strncmp(a,b,5)==0)
 	}
 }
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	while (1)
+	{
+		char a[100];
+		char b[100];
+		int i;
+		printf("请输入一个字符串：");
+		scanf_s("%s", a, 100);
+		for (i = 0; i < strlen(a); i++)
+		{
+			b[i] = a[strlen(a) - 1 - i];
+		}
+		b[i] = '\0';
+		if (strcmp(a, b) == 0)
+			printf("您输入的字符串是回文字符串。\n");
+		else printf("您输入的字符串不是回文字符串。\n");
+	}
+
+}
