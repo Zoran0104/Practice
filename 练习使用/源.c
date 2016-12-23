1f528dd39ff8fb01ee75fe2325f006c9880a2572
Zoran <zwyliufeng@outlook.com>
2016-12-23 15:52:37 +0800

    15

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,49 +1,42 @@
 ﻿#include<stdio.h>
-#define SIZE 20 
-int column; 
-int row;
-void inputMatrix(int a[][SIZE], int n, int m) { 
-	int i, j;
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < m; j++) {
-			scanf("%d", &a[i][j]);
-		}
-	}
-}
-
-void outputMatrix(int b[][SIZE], int n, int m) {
-	int i, j;
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < m; j++) {
-			printf("%d ", b[i][j]);
-		}
-		printf("\n"); 
-	}
-}
-
-void matrixTransport(int a[][SIZE], int b[][SIZE]) {
-	int i, j;
-	for (i = 0; i < row; i++) {
-		for (j = 0; j < column; j++) {
-			b[i][j] = a[j][i];
-
-		}
-	}
-}
-int main()
+int jia(int x, int y);
+int jian(int x, int y);
+int cheng(int x, int y);
+int chu(int x, int y);
+int main(void)
 {
+	while (1)
+	{
+		int x;
+		int y;
+		scanf("%d", &x);
+		scanf("%d", &y);
+		jia(x, y);
+		jian(x, y);
+		cheng(x, y);
+		chu(x, y);
+	}
+}
 
-	int a[SIZE][SIZE] = { 0 };
-	int b[SIZE][SIZE] = { 0 };
-	printf("请输入您要进行转置的矩阵的行数 : column =  ");
-	scanf("%d", &column);
-	printf("\n请输入您要进行转置的矩阵的列数 : row = ");
-	scanf("%d", &row);
-	printf("请输入一个 %d X %d 的矩阵 \n", column, row);
-	inputMatrix(a, column, row);
-	matrixTransport(a, b);
-	printf("转置后的矩阵是 :\n");
-	outputMatrix(b, row, column);
-	getchar();
+int jia(int x, int y)
+{
+	printf("两个数相加的结果是：%d\n", x + y);
+}
+
+int jian(int x, int y)
+{
+	printf("两个数相减的结果为%d\n", x - y);
 	return 0;
+}
+
+int cheng(int x, int y)
+{
+	printf("两个数相乘的结果为%d\n", x*y);
+}
+
+int chu(int x, int y)
+{
+	if (y)
+		printf("两个数相除的结果为%.2lf\n", (double)x / (double)y);
+	else printf("0不能作除数。\n");
 }
