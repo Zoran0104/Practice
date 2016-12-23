a8b212e68131db0932063dd6a8d1481f677754af
Zoran <zwyliufeng@outlook.com>
2016-12-23 17:54:18 +0800

    1

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,42 +1,31 @@
 ﻿#include<stdio.h>
-int jia(int x, int y);
-int jian(int x, int y);
-int cheng(int x, int y);
-int chu(int x, int y);
+void myswap1(int x, int y);
+void myswap2(int *p, int *q);
 int main(void)
 {
-	while (1)
-	{
-		int x;
-		int y;
-		scanf("%d", &x);
-		scanf("%d", &y);
-		jia(x, y);
-		jian(x, y);
-		cheng(x, y);
-		chu(x, y);
-	}
+	int x = 3;
+	int y = 5;
+	printf("before:x=%d,y=%d\n", x, y);
+	myswap1(x, y);
+	printf("after:x=%d,y=%d", x, y);
+	myswap2(&x, &y);
+	printf("after:x=%d,y=%d", x, y);
+	system("pause");
 }
 
-int jia(int x, int y)
+void myswap1(int x, int y)
 {
-	printf("两个数相加的结果是：%d\n", x + y);
+	int z = 0;
+	z = x;
+	x = y;
+	y = z;
+
 }
 
-int jian(int x, int y)
+void myswap2(int *p, int *q)
 {
-	printf("两个数相减的结果为%d\n", x - y);
-	return 0;
-}
-
-int cheng(int x, int y)
-{
-	printf("两个数相乘的结果为%d\n", x*y);
-}
-
-int chu(int x, int y)
-{
-	if (y)
-		printf("两个数相除的结果为%.2lf\n", (double)x / (double)y);
-	else printf("0不能作除数。\n");
+	int z = 0;
+	z = *p;
+	*p = *q;
+	*q = z;
 }
