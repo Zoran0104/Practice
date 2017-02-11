6f7bf5132036b3d7b2b46b80c2100b010729009c
Zoran <zwyliufeng@outlook.com>
2017-02-11 16:51:53 +0800

    1651

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,17 +1,20 @@
 ﻿#include<stdio.h>
 #include<math.h>
 int to_ten(char num[], int jz);
+void to_what(int n, int mjz);
 int main()
 {
 	while (1)
 	{
 		char num[100];
-		int jz;
+		int jz=0,mjz=0;
 		printf("请输入一个数：");
 		scanf("%s", num);
 		printf("请输入这个数的进制：");
 		scanf("%d", &jz);
-		to_ten(num, jz);
+		printf("请输入目标进制：");
+		scanf("%d", &mjz);
+		to_what(to_ten(num, jz), mjz);
 	}
 }
 
@@ -34,5 +37,27 @@ int to_ten(char num[], int jz)
 		p = pow(jz, n - i - 1);
 		sum += temp * p;
 	}
-	printf("%d\n", sum);
+	return sum;
+}
+
+void to_what(int n, int mjz)
+{
+	if (mjz == 10)
+		return n;
+	char num[100];
+	int j=0;
+	while (n > mjz)
+	{
+		if (n%mjz > 9)
+			num[j++] = n%mjz - 10 + 'A';
+		else
+			num[j++] = n%mjz + 48;
+		n /= mjz;
+	}
+	if (n > 9)
+		num[j] = n - 10 + 'A';
+	else num[j] = n + 48;
+	for (int i = j; i != -1; i--)
+		putchar(num[i]);
+	putchar('\n');
 }
