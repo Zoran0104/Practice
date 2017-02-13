4fb90e907ad01c35d00448f621894907840bffee
Zoran <zwyliufeng@outlook.com>
2017-02-13 15:51:29 +0800

    1551

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,30 +1,33 @@
 ﻿#include<stdio.h>
 #include<stdbool.h>
-#define MAX_Lenth 1001
+#define NUM 1001
 int main()
 {
-	bool prime[MAX_Lenth] = { false };
-	for (int i = 2; i != MAX_Lenth; ++i)
+	bool prime[NUM] = { false };
+	for (int i = 2; i < NUM; ++i)
+	{
 		prime[i] = true;
-	for (int i = 2; i*i < MAX_Lenth; ++i)
+	}
+	for (int i = 2; i*i < NUM; ++i)
 	{
 		if (!prime[i]) continue;
-		for (int j = i*i; j < MAX_Lenth; ++j)
-			if (j%i == 0) prime[j] = false;
+		for (int j = i*i; j < NUM; ++j)
+			if (j%i == 0)
+				prime[j] = false;
 	}
 	int n = 1;
-	for (int i = 2; i < MAX_Lenth; ++i)
+	for (int i = 2; i < NUM; ++i)
 	{
 		if (prime[i])
 		{
-			printf("%6d", i);
+			printf("%5d", i);
 			++n;
 		}
 		if (n > 10)
 		{
-			printf("\n");
+			putchar('\n');
 			n = 1;
 		}
 	}
-	while (1);
+	system("pause");
 }
