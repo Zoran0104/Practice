29801a26e9e983b38fe3c3e09ae99636971cd5c2
Zoran <zwyliufeng@outlook.com>
2017-02-14 17:50:48 +0800

    1750

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,32 +1,43 @@
 ﻿#include<stdio.h>
 #include<stdbool.h>
-#define NUMMAX 10001
+void isprime(int n, bool num[])
+{
+	num[0] = num[1] = false;
+	for (int i = 2; i < n + 1; ++i)
+		num[i] = true;
+	for (int i = 2; i*i < n + 1; ++i)
+		if (num[i])
+			for (int j = i*i; j < n; ++j)
+				if (j%i == 0)
+					num[j] = false;
+}
 int main()
 {
-	bool prime[NUMMAX] = { false };
-	for (int i = 2; i < NUMMAX; ++i)
-		prime[i] = true;
-	for (int i = 2; i*i <= NUMMAX; ++i)
+	int flag;
+	printf("哥德巴赫猜想验证：");
+	printf("\n请输入您想验证的最大整数：");
+	int n=0;
+	scanf("%d", &n);
+	bool *p;
+	if (!(p = (bool *)malloc(sizeof(bool)*n)))
 	{
-		if (!prime[i]) continue;
-		for (int j = i*i; j < NUMMAX; ++j)
-			if (j%i == 0) prime[j] = false;
+		printf("wrong to run");
+		system("pause");
 	}
-	int n = 0,ls=0;
-	for (int i = 2; i < NUMMAX; ++i)
+	isprime(n, p);
+	for (int i = 6; i <= n; i += 2)
 	{
-		if (prime[i])
+		flag = 1;
+		for (int j = 2; j <= i / 2; ++j)
 		{
-			++n;
-			if (prime[i] && prime[i - 2])
+			if (j % 2 == 0||(i-j)%2==0) continue;
+			if (p[j] && p[i - j])
 			{
-				printf("(%3d,%3d)     ", i-2, i);
-				++ls;
-				if (ls % 5 == 0)
-					putchar('\n');
+				printf("%d=%d+%d\n", i, j, i - j);
+				flag = 0;
 			}
 		}
+		flag == 1 ? printf("found wrong number") : 1;
 	}
-	printf("\n%d数内有%d个素数，有%d个孪生素数。\n", NUMMAX - 1, n, ls);
 	system("pause");
 }
