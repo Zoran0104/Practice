29ad3808788210d6b055fbd7d32bb9b358ae1d99
Zoran <zwyliufeng@outlook.com>
2017-02-17 10:23:47 +0800

    1023

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,42 +1,38 @@
 ﻿#include<stdio.h>
 #include<stdbool.h>
-void isprime(int n, bool num[]);
+void isprime(int num, bool a[]);
+
 int main()
 {
-	int n = 0;
-	printf("验证哥的猜想：\n请输入数的范围：");
-	scanf("%d", &n);
-	bool *p;
-	p = (bool *)malloc(sizeof(bool)*n);
-	isprime(n, p);
-	for (int i = 6; i <= n; i += 2)
+	printf("Please enter a number the max");
+	int num;
+	scanf("%d", &num);
+	bool *prime=0;
+	prime = (bool *)malloc(sizeof(bool)*num);
+	isprime(num, prime);
+	system("pause");
+}
+
+void isprime(int num, bool a[])
+{
+	a[0] = a[1] = false;
+	for (int i = 2; i <= num; ++i)
+		a[i] = true;
+	for (int i = 2; i*i <= num; ++i)
+		if (a[i])
+			for (int j = i*i; j <= num; ++j)
+				if (j%i == 0)
+					a[j] = false;
+	for(int i=6;i<=num;i+=2)
 	{
 		int flag = 1;
-		for (int j = 2; j <= i / 2; ++j)
+		for (int j = 2; j <= i/2; ++j)
 		{
-			if (p[j] && p[i - j])
+			if (a[j] && a[i - j])
 			{
 				printf("%d=%d+%d\n", i, j, i - j);
 				flag = 0;
-			}
-		}
-		if (flag == 1)
-			printf("wrong number!");
-	}
-	system("pause");
-}
-void isprime(int n, bool num[])
-{
-	num[1] = num[0] = false;
-	for (int i = 2; i < n; ++i)
-		num[i] = true;
-	for (int i = 2; i*i <= n; ++i)
-	{
-		if (num[i])
-		{
-			for (int j = i*i; j < n; ++j)
-			{
-				if (j%i == 0) num[j] = false;
+				break;
 			}
 		}
 	}
