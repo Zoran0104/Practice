162d947bede1f953109f9a25efc7e6a2318715e6
Zoran <zwyliufeng@outlook.com>
2017-03-04 13:13:38 +0800

    1313

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,28 +1,38 @@
 ﻿#include<stdio.h>
-#include<stdbool.h>
-void Prime(bool *p, int n);
+struct Famil
+{
+	char name[10];
+	int age;
+};
 int main()
 {
-	bool *p;
-	int n;
-	printf("Please enter the max number you wanted:");
-	scanf("%d", &n);
-	p = (bool *)malloc(sizeof(bool)*n);
-	Prime(p, n);
+	FILE *p=NULL;
+	int max = 0,min=0;
+	struct Famil FAM[100];
+	p=fopen("d:\\home.txt", "w");
+	if (p == NULL) return -1;
+	printf("请输入你的家庭成员信息：(以‘| 0’结束)\n");
+	scanf("%s %d", FAM[0].name, &FAM[0].age);
+	int i = 0;
+	while(FAM[i].name[0]!='|')
+	{
+		fprintf(p, "%s %d\n", FAM[i].name, FAM[i].age);
+		scanf("%s %d", FAM[++i].name, &FAM[i].age);
+	}
+	fclose(p);
+	i = 0;
+	p=fopen("d:\\home.txt", "r");
+	if (p == NULL) return -1;
+	while(!feof(p))
+	{
+		fscanf(p, "%s %d\n", FAM[i].name, &FAM[i].age);
+		printf("%s %d\n", FAM[i].name, FAM[i].age);
+		max = (FAM[max].age > FAM[i].age) ? max : i;
+		min = (FAM[min].age > FAM[i].age) ? i : min;
+		++i;
+	}
+	fclose(p);
+	printf("年龄最大的是%s,TA的年龄是%d\n年龄最小的是%s,TA的年龄是%d\n"
+		, FAM[max].name, FAM[max].age,FAM[min].name,FAM[min].age);
 	system("pause");
-}
-void Prime(bool *p, int n)
-{
-	p[1] = p[0] = false;
-	for (int i = 2; i <= n; ++i)
-		p[i] = true;
-	for (int i = 2; i*i <= n;++i)
-		for (int j = i*i; j <= n; ++j)
-			if (p[j])
-				if (j%i == 0)
-					p[j] = false;
-	for (int i = 2; i <= n; ++i)
-		if (p[i])
-			printf("%5d", i);
-
 }
