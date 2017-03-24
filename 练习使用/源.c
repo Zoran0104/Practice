fecc41b4752bb3552907d49172f136c2d0cece3e
Zoran <zwyliufeng@Outlook.com>
2017-03-25 01:41:15 +0800

    325

--- 练习使用/源.c
+++ 练习使用/源.c
@@ -1,11 +1,15 @@
 ﻿#include<stdbool.h>
 #include<stdio.h>
 #include<string.h>
-void Find_Base_Num(struct Stu_info a[], char num[]);
-void Find_Base_Name(struct Stu_info a[], char num[]);
-void Find_Base_Tel(struct Stu_info a[], char num[]);
-void Find_Base_Sex(struct Stu_info a[], char num[]);
-void Find_Base_Birth(struct Stu_info a[], struct Stu_birth b, struct Stu_birth c);
+int Get_Length();
+void Find_Base_Num(char num[]);
+void Find_Base_Name(char num[]);
+void Find_Base_Tel(char num[]);
+void Find_Base_Sex(char num[]);
+void Find_Base_Birth(struct Stu_birth b, struct Stu_birth c);
+void Delete_Main();
+void Delete_Base_Num();
+void Add_One_info();
 bool Check_Num(char a[]);
 bool Check_Name(char a[]);
 bool Check_Sex(char a[]);
@@ -13,7 +17,7 @@ bool Check_Tel(char a[]);
 bool Check_Birth(int years, int months, int days);
 void Get_info();
 void Print_Only1_info(struct Stu_info a);
-void Print_info(struct Stu_info a[]);
+void Print_info();
 
 struct Stu_birth
 {
@@ -29,30 +33,36 @@ struct Stu_info
 	struct Stu_birth birth;
 	char Tel[11];
 };
-typedef struct Stu_info info;
+struct Stu_info Info[1000];
 int main()
 {
 	Get_info();
 }
 
-void Find_Base_Num(struct Stu_info a[], char num[])
+int Get_Length()
+{
+	int i=0;
+	for (; Info[i].sex == "T" || Info[i].sex == "F"; ++i);
+	return i;
+}
+
+void Find_Base_Num(char num[])
 {
 	int k;
 	int flag = 0;
-	for (int i = 0; a[i].sex == "T" || a[i].sex == "F"; ++i)
+	int n = Get_length();
+	for (int i = 0; i<n ; ++i)
 	{
-		for (int j = 0; j <= strlen(a[i].cNum) - strlen(num); ++j)
+		for (int j = 0; j <= strlen(Info[i].cNum) - strlen(num); ++j)
 		{
 			for (k = 0; num[k] != 0; ++k)
 			{
-				if (a[i].cNum[j + k] != num[k])
+				if (Info[i].cNum[j + k] != num[k])
 					break;
 			}
 			if (num[k] == 0)
 			{
-				printf("%s %s %s %d-%d-%d %s", a[i].cNum, a[i].cName, a[i].sex,
-					a[i].birth.years, a[i].birth.months, a[i].birth.days,
-					a[i].Tel);
+				Print_Only1_info(Info[i]);
 				break;
 				flag = 1;
 			}
@@ -61,25 +71,24 @@ void Find_Base_Num(struct Stu_info a[], char num[])
 	if (flag == 0)
 		printf("没有从数据中找到相应的结果！");
 }
-void Find_Base_Name(struct Stu_info a[], char name[])
+void Find_Base_Name(char name[])
 {
 	int k;
 	int flag = 0;
-	for (int i = 0; a[i].sex == "T" || a[i].sex == "F"; ++i)
+	int n = Get_length();
+	for (int i = 0; i < n; ++i)
 	{
-		for (int j = 0; j <= strlen(a[i].cName) - strlen(name); ++j)
+		for (int j = 0; j <= strlen(Info[i].cName) - strlen(name); ++j)
 		{
 			for (k = 0; name[k] != 0; ++k)
 			{
-				if (a[i].cName[j + k] != name[k])
+				if (Info[i].cName[j + k] != name[k])
 					break;
 			}
 			if (name[k] == 0)
 			{
 				flag = 1;
-				printf("%s %s %s %d-%d-%d %s", a[i].cNum, a[i].cName, a[i].sex,
-					a[i].birth.years, a[i].birth.months, a[i].birth.days,
-					a[i].Tel);
+				Print_Only1_info(Info[i]);
 				break;
 			}
 		}
@@ -88,25 +97,24 @@ void Find_Base_Name(struct Stu_info a[], char name[])
 		printf("没有根据搜索条件找到结果！");
 }
 
-void Find_Base_Tel(struct Stu_info a[], char Tel[])
+void Find_Base_Tel(char Tel[])
 {
 	int k;
 	int flag = 0;
-	for (int i = 0; a[i].sex == "T" || a[i].sex == "F"; ++i)
+	int n = Get_length();
+	for (int i = 0; i < n; ++i)
 	{
-		for (int j = 0; j <= strlen(a[i].Tel) - strlen(Tel); ++j)
+		for (int j = 0; j <= strlen(Info[i].Tel) - strlen(Tel); ++j)
 		{
 			for (k = 0; Tel[k] != 0; ++k)
 			{
-				if (a[i].Tel[j + k] != Tel[k])
+				if (Info[i].Tel[j + k] != Tel[k])
 					break;
 			}
 			if (Tel[k] == 0)
 			{
 				flag = 1;
-				printf("%s %s %s %d-%d-%d %s", a[i].cNum, a[i].cName, a[i].sex,
-					a[i].birth.years, a[i].birth.months, a[i].birth.days,
-					a[i].Tel);
+				Print_Only1_info(Info[i]);
 				break;
 			}
 		}
@@ -115,69 +123,59 @@ void Find_Base_Tel(struct Stu_info a[], char Tel[])
 		printf("没有根据搜索条件找到结果！");
 }
 
-void Find_Base_Sex(struct Stu_info a[], char Sex[])
+void Find_Base_Sex(char Sex[])
 {
 	int flag = 0;
-	for (int i = 0; a[i].sex == "T" || a[i].sex == "F"; ++i)
+	int n = Get_length();
+	for (int i = 0; i < n; ++i)
 	{
-		if (a[i].sex[0] == Sex[0])
+		if (Info[i].sex[0] == Sex[0])
 		{
 			flag = 1;
-			printf("%s %s %s %d-%d-%d %s", a[i].cNum, a[i].cName, a[i].sex,
-				a[i].birth.years, a[i].birth.months, a[i].birth.days,
-				a[i].Tel);
+			Print_Only1_info(Info[i]);
 		}
 	}
 	if (flag == 0)
 		printf("没有根据搜索条件找到结果！");
 }
-void Find_Base_Birth(struct Stu_info a[], struct Stu_birth b, struct Stu_birth c)
+void Find_Base_Birth(struct Stu_birth b, struct Stu_birth c)
 {
 	int flag = 0;
-	for (int i = 0; a[i].sex == "T" || a[i].sex == "F"; ++i)
+	int n = Get_length();
+	for (int i = 0; i < n; ++i)
 	{
-		if (a[i].birth.years > b.years&&a[i].birth.years < c.years)
+		if (Info[i].birth.years > b.years&&Info[i].birth.years < c.years)
 		{
 			flag = 1;
-			printf("%s %s %s %d-%d-%d %s", a[i].cNum, a[i].cName, a[i].sex,
-				a[i].birth.years, a[i].birth.months, a[i].birth.days,
-				a[i].Tel);
+			Print_Only1_info(Info[i]);
 			continue;
 		}
-		else if (a[i].birth.years == b.years)
+		else if (Info[i].birth.years == b.years)
 		{
-			if (a[i].birth.months > b.months)
+			if (Info[i].birth.months > b.months)
 			{
-				printf("%s %s %s %d-%d-%d %s", a[i].cNum, a[i].cName, a[i].sex,
-					a[i].birth.years, a[i].birth.months, a[i].birth.days,
-					a[i].Tel);
+				Print_Only1_info(Info[i]);
 				flag = 1;
 				continue;
 			}
-			else if (a[i].birth.months == b.months&&a[i].birth.days >= b.days)
+			else if (Info[i].birth.months == b.months&&Info[i].birth.days >= b.days)
 			{
-				printf("%s %s %s %d-%d-%d %s", a[i].cNum, a[i].cName, a[i].sex,
-					a[i].birth.years, a[i].birth.months, a[i].birth.days,
-					a[i].Tel);
+				Print_Only1_info(Info[i]);
 				flag = 1;
 				continue;
 			}
 		}
-		else if (a[i].birth.years == c.years)
+		else if (Info[i].birth.years == c.years)
 		{
-			if (a[i].birth.months < c.months)
+			if (Info[i].birth.months < c.months)
 			{
-				printf("%s %s %s %d-%d-%d %s", a[i].cNum, a[i].cName, a[i].sex,
-					a[i].birth.years, a[i].birth.months, a[i].birth.days,
-					a[i].Tel);
+				Print_Only1_info(Info[i]);
 				flag = 1;
 				continue;
 			}
-			else if (a[i].birth.months == c.months&&a[i].birth.days <= b.days)
+			else if (Info[i].birth.months == c.months&&Info[i].birth.days <= b.days)
 			{
-				printf("%s %s %s %d-%d-%d %s", a[i].cNum, a[i].cName, a[i].sex,
-					a[i].birth.years, a[i].birth.months, a[i].birth.days,
-					a[i].Tel);
+				Print_Only1_info(Info[i]);
 				flag = 1;
 				continue;
 			}
@@ -185,6 +183,71 @@ void Find_Base_Birth(struct Stu_info a[], struct Stu_birth b, struct Stu_birth c
 	}
 	if (flag == 0)
 		printf("没有根据搜索条件找到结果！");
+}
+void Delete_Main()
+{
+	int i;
+	printf("     |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
+	printf("     1.根据学号删除\n");
+	printf("     2.根据姓名删除\n");
+	printf("     3.根据手机号码删除\n");
+	printf("     4.根据出生日期删除\n");
+	printf("     |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|\n");
+	printf("请选择您的删除方式：\n");
+	start:
+	scanf("%d", &i);
+	switch (i)
+	{
+	case 1:
+		Delete_Base_Num();
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	default:
+		printf("您输入的选择无效，请重新输入：");
+		goto start;
+	}
+
+
+
+}
+void Delete_Base_Num()
+{
+	printf("请输入要删除的学号：");
+	char Num[100];
+	scanf("%s", Num);
+	int n = Get_Length();
+	int i;
+	for (i= 0; i < n; ++i)
+	{
+		
+	}
+}
+void Add_One_info()
+{
+	int i =	Get_Length();
+	printf("请输入这位同学的学号：");
+	while (scanf("%s", Info[i].cNum) == 1 && !Check_Num(Info[i].cNum))
+		printf("您输入的学号不符合要求，请重新输入：");
+	printf("请输入这位同学的姓名：");
+	while (scanf("%s", Info[i].cName) == 1 && !Check_Name(Info[i].cName))
+		printf("您输入的姓名不符合要求，请重新输入：");
+	printf("请输入这位同学的性别：");
+	while (scanf("%s", Info[i].sex) && !Check_Sex(Info[i].sex))
+		printf("您输入的性别不符合要求，请重新输入：");
+	printf("请输入这位同学的出生年月日：（请按xxxx-xx-xx格式输入）");
+	while (scanf("%d-%d-%d", &Info[i].birth.years,
+		&Info[i].birth.months,
+		&Info[i].birth.days) && !Check_Birth(Info[i].birth.years,
+			Info[i].birth.months, Info[i].birth.days))
+		printf("您输入的日期不符合要求，请重新输入：");
+	printf("请输入这位同学的手机号码：");
+	while (scanf("%s", Info[i].Tel) && !Check_Tel(Info[i].Tel))
+		printf("您输入的手机号码不符合要求，请重新输入：");
 }
 bool Check_Num(char a[])
 {
@@ -248,7 +311,7 @@ void Get_info()
 	int stunum = 0;
 	while (scanf("%d", &stunum) == 1 && (stunum <= 0 || stunum > 1000))
 		printf("您输入的数小于0或大于1000，请重新输入：");
-	info Info[1000];
+
 	for (int i = 0; i < stunum; ++i)
 	{
 		printf("请输入第%d位同学的学号：", i + 1);
@@ -260,8 +323,8 @@ void Get_info()
 		printf("请输入第%d位同学的性别：", i + 1);
 		while (scanf("%s", Info[i].sex) && !Check_Sex(Info[i].sex))
 			printf("您输入的性别不符合要求，请重新输入：");
-		printf("请输入第%d位同学的出生年月日：", i + 1);
-		while (scanf("%d%d%d", &Info[i].birth.years,
+		printf("请输入第%d位同学的出生年月日：（请按xxxx-xx-xx格式输入）", i + 1);
+		while (scanf("%d-%d-%d", &Info[i].birth.years,
 			&Info[i].birth.months,
 			&Info[i].birth.days) && !Check_Birth(Info[i].birth.years,
 				Info[i].birth.months, Info[i].birth.days))
@@ -278,12 +341,12 @@ void Print_Only1_info(struct Stu_info a)
 		a.birth.months, a.birth.days, a.Tel);
 }
 
-void Print_info(struct Stu_info a[])
+void Print_info()
 {
 	int i = 0;
-	for (i = 0; a[i].sex == "T" || a[i].sex == "F"; ++i)
+	for (i = 0; Info[i].sex == "T" || Info[i].sex == "F"; ++i)
 	{
-		Print_Only1_info(a[i]);
+		Print_Only1_info(Info[i]);
 	}
 	if (i == 0)
 	{
